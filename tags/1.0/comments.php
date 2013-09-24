6a645cfd14cd8e674f268d6d29a5fe2b17e21d31
Vicomi <Vicomi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-24 10:07:26 +0000

    first tag 1.0
    git-svn-id: https://plugins.svn.wordpress.org/vicomi-comment-system@777661 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0/comments.php
@@ -0,0 +1,5 @@
+<div id="vicomi_comments">
+	<div id="vc-recommend-iframe-wrapper" style="height:300px;"></div>
+	<div id="vc-comments-iframe-wrapper"></div>
+	<script type="text/javascript" src="http://api.vicomi.com/embed/widgets.js?access_token=<?php echo get_option('vicomi_api_key'); ?>"></script>
+</div>
